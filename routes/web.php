5f023943db8edbf439921041fe63f9a3e3734cf7
yutaro101 <yutaro10120424@gmail.com>
2021-10-19 21:02:21 +0900

    カリキュラム8-1

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'PostController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/posts/{post}', 'PostController@show');
